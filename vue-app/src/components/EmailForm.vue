d789894e8f20dc1c33413c70fe79d74b294a15fc
Raul <raulwwq0@gmail.com>
2021-03-17 18:17:37 +0100

    Divide contact in email form and social components

---
+++ vue-app/src/components/EmailForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <form
+      id="email-form"
+      method="POST"
+      action="send"
+      enctype="multipart/form-data"
+    >
+      <p class="first-p">Usa este formulario para contactarme</p>
+      <input type="text" name="name" placeholder="Nombre" required />
+      <input type="text" name="email" placeholder="Correo" required />
+      <input type="text" name="subject" placeholder="Asunto" required />
+      <textarea name="message" placeholder="Mensaje" required />
+      <input type="submit" value="Enviar" />
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'EmailForm',
+}
+</script>
