994ae9418dfe4f982fc5bf91d0b38fe643193a34
Raul <raulwwq0@gmail.com>
2021-03-21 19:29:35 +0100

    Adding img, update home page and minor changes

---
+++ vue-app/src/components/Home/AboutMe.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="about-me">
+    <h2 class="subtitle">Sobre mi</h2>
+    <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu urna
+    pretium, ultricies odio nec, finibus odio. Nunc maximus leo dolor. Aliquam
+    placerat velit ut rhoncus ultrices. Sed tempus aliquet ornare. Suspendisse
+    viverra suscipit elementum. Aliquam ultrices vulputate rutrum. Quisque
+    commodo viverra nulla, ac efficitur arcu vehicula sodales. Nunc dignissim
+    at leo a suscipit. Pellentesque malesuada scelerisque mauris, ut gravida
+    metus feugiat ac.
+    </p>
+    <img src="https://via.placeholder.com/200" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutMe",
+};
+</script>
