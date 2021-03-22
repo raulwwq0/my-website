229d1d36e7fb94fd1f30b4b66cbea599b4b37ce3
Raul <raulwwq0@gmail.com>
2021-03-22 20:11:47 +0100

    Creating the background grid and the animation

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <Sidebar />
+  <Background />
   <div class="content">
     <router-view />
   </div>
@@ -8,11 +9,13 @@
 <script>
 import './assets/styles/css/styles.css';
 import Sidebar from './components/Sidebar';
+import Background from './components/Background';
 
 export default {
   name: 'App',
   components: {
     Sidebar,
+    Background,
   }
 }
 </script>
