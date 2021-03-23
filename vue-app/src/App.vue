80cc8d919a2c6373fd3975a3de0c3aba799e930d
Raul <raulwwq0@gmail.com>
2021-03-23 11:21:14 +0100

    Remove glass class in more browsers

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -18,14 +18,17 @@ export default {
     Background,
   },
   setup() {
-    // This function remove glass effect in firefox due to backdrop-filters are not supported by default
+    // This function remove glass effect in browsers where backdrop-filters are not supported by default
     function whichNavIsUsed(){
       document.addEventListener("DOMContentLoaded", () => {
         const firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+        const opera = navigator.userAgent.toLowerCase().indexOf('opera') > -1;
+        const internetExplorer = navigator.userAgent.toLowerCase().indexOf('msie') > -1;
+
         const glassClass = document.getElementsByClassName("glass");
         console.log(Array.from(glassClass));
 
-        if(firefox){
+        if(firefox || opera || internetExplorer){
           Array.from(glassClass).forEach((element) => {
             element.classList.remove('glass');
             element.classList.add('no-glass');
