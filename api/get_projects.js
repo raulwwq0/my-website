6c9381de766678302550b4f883de03c5c43bf5dd
Raúl Aguilar <ag21raul@gmail.com>
2021-10-21 22:51:55 +0200

    experiment with routes

--- api/get_projects.js
+++ api/get_projects.js
@@ -1,16 +1,5 @@
 'use strict'
 
-var express = require('express');
-
-var ProjectController = require('../controllers/project');
-
-var router = express.Router();
-
-// Connect-multiparty configuration
-var multipart = require('connect-multiparty');
-var md_upload = multipart({uploadDir: './upload/projects'});
-
-router.get('/get-projects', ProjectController.getProjects);
-
-
-module.exports = router;
+module.exports = (req, res) => {
+    res.send('Hello world from the /api route!');
+  };
